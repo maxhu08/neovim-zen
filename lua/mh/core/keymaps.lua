71e76b617508b63b8899001cde02ab318fcac75d
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-04-04 20:49:59 -0500

    change keymap since sv didnt work

--- lua/mh/core/keymaps.lua
+++ lua/mh/core/keymaps.lua
@@ -12,8 +12,8 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "increment number" })
 keymap.set("n", "<leader>-", "<C-a>", { desc = "decrement number" })
 
 -- spliting windows
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "split window vertically" })
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "split window horizontally" })
+keymap.set("n", "<leader>ss", ":vsplit<CR>", { desc = "split window vertically" })
+keymap.set("n", "<leader>sh", ":split<CR>", { desc = "split window horizontally" })
 keymap.set("n", "<leader>sv", "<C-w>=", { desc = "make windows equal size" })
 keymap.set("n", "<leader>sl", "<cmd>close<CR>", { desc = "close current split" })
 
