d031f0fa064f5a6048834e7e1a0efe2d4c765c77
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-04-04 20:57:16 -0500

    add dressing and vim-maximizer

--- lua/mh/lazy.lua
+++ lua/mh/lazy.lua
@@ -12,6 +12,10 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("mh.plugins", {
+  checker = {
+    enabled = true,
+    notifty = false
+  },
   change_detection = {
     notify = false
   }
