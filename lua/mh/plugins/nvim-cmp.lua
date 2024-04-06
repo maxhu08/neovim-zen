6d1b9d825a077a4a9b6345e4a3953d5026462642
maxhu08 <140214354+maxhu08@users.noreply.github.com>
2024-04-05 20:46:07 -0500

    make nvim-cmp auto select first option

--- lua/mh/plugins/nvim-cmp.lua
+++ lua/mh/plugins/nvim-cmp.lua
@@ -27,7 +27,8 @@ return {
 
 		cmp.setup({
 			completion = {
-				completeopt = "menu,menuone,preview,noselect",
+				-- add noselect to not select first option
+				completeopt = "menu,menuone,preview",
 			},
 			snippet = { -- configure how nvim-cmp interacts with snippet engine
 				expand = function(args)
@@ -39,7 +40,7 @@ return {
 				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
 				-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<C-Space>"] = cmp.mapping.complete(),
 				-- ["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
